4aed02fa4dcbf4ae0f5c6b029a8dad949009fda3
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 19:36:07 +0300

    Add HomeWork_2\3

---
+++ HomeWork_2/3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите цифру - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 7 || number <= 0 )
+{
+    Console.WriteLine("Ввод недопустимого значения!");
+}
+else
+{
+    if (number >= 6)
+    {
+        Console.WriteLine("День является выходным!");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
