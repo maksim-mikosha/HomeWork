d6bc9d2511abe97fb4e9540f10f0e72fde363c18
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 18:04:12 +0300

    Add homework_2

---
+++ 2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите первое число - ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ваши числа - " + numberA + " " + numberB);
+
+int maxNumber = numberA;
+
+if (numberA == numberB)
+{
+    Console.WriteLine("Введенные числа равны!");
+}
+else
+{
+    if (maxNumber < numberB)
+    {
+        maxNumber = numberB;
+    }
+    Console.WriteLine("Наибольшее число " + maxNumber);
+}
