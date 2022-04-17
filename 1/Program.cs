ccfdc4b527c24e63d7d63dd91a3c70586bfa0de4
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 17:40:16 +0300

    Add homework_1

---
+++ 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ваше число - " + number);
+
+void Result(int num)
+{
+    int divNumbers100 = num / 100;
+    int divNumbers10 = num % 10;
+    int secondNumber = (num - divNumbers100 * 100 - divNumbers10) / 10;
+   
+    Console.WriteLine("Вторая цифра - " + secondNumber);
+   
+}
+
+Result(number);
