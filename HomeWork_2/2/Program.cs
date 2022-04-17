d6330fdb2d2e72663330f1865be4adfb0b6f3280
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 19:23:46 +0300

    Add HomeWork_2\2

---
+++ HomeWork_2/2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());               //Ввод данных так потому что string не допускает значение NULL, VS Code проинформировал
+string stringNumber = Convert.ToString(number);
+
+Console.WriteLine("Ваше число - " + stringNumber);
+
+if (stringNumber.Length <= 2)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    Console.WriteLine("Третья цифра - " + stringNumber[2]);
+}
