2d5d3af9edca9868b8314bd0f2f9e82a9c153fbd
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 19:00:36 +0300

    Change HomeWork_1\1

--- HomeWork_1/1/Program.cs
+++ HomeWork_1/1/Program.cs
@@ -3,14 +3,6 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Ваше число - " + number);
 
-void Result(int num)
-{
-    int divNumbers100 = num / 100;
-    int divNumbers10 = num % 10;
-    int secondNumber = (num - divNumbers100 * 100 - divNumbers10) / 10;
-   
-    Console.WriteLine("Вторая цифра - " + secondNumber);
-   
-}
+int divNumbers10 = number % 10;
 
-Result(number);
+Console.WriteLine("Последняя цифра - " + divNumbers10);
