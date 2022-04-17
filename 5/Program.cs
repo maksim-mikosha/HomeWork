36b726ccec99030086e006e14e514928226d0fcd
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 18:42:40 +0300

    Add homework_5

---
+++ 5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ваше число - " + number);
+
+int index = 2;
+
+if (number < index)
+{
+    Console.WriteLine("Отсутствуют четные числа или введено отрицательное число!");
+}
+else
+{
+    Console.Write("Четные числа от 1 до " + number + " - ");
+    while (number >= index)
+    {
+        Console.Write(index + " ");
+        index += 2;
+    }
+}
