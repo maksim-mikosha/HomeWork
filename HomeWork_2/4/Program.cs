2008ad9ef2316f42c9b5834bfd8d18e514043955
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 20:49:16 +0300

    Add HomeWork_2\4

---
+++ HomeWork_2/4/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("--==Игра Угадайка!==--");
+Console.WriteLine("Загадываю случайное число");
+
+int randomNumber = new Random().Next(10, 1000);
+int countTry = 1;
+
+while (countTry <= 3)
+{
+    Console.Write("Угадывай! Попытка № " + countTry + " - ");
+    int userNumber = Convert.ToInt32(Console.ReadLine());
+    if (userNumber == randomNumber)
+    {
+        Console.WriteLine("Поздравляю! Ты справился :)");
+        break;
+    }
+    else
+    {
+        Check(randomNumber, userNumber);
+    }
+    countTry++;
+}
+
+if (countTry > 3)
+{
+    Console.WriteLine("Попытки закончились. Удачи в следующий раз.");
+    Console.WriteLine("Загаданное число - " + randomNumber);
+}
+
+void Check(int numberA, int numberB)
+{
+    if (numberA > numberB)
+    {
+        Console.WriteLine("Загаданное число больше");
+    }
+    else
+    {
+        Console.WriteLine("Загаданное число меньше");
+    }
+}
