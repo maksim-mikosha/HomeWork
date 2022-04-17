aa21e493e5da2b94e8e28252138aa0d1b76f2b8f
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 18:27:02 +0300

    Add homework_4

---
+++ 4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ваше число - " + number);
+
+int divNumber = number % 2;
+
+if (divNumber == 1)
+{
+    Console.WriteLine("Введенное число не чётное!");
+}
+else
+{
+    Console.WriteLine("Введенное число чётное!");
+}
