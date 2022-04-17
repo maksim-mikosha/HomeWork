ef2c5ed93a02d56c81baeff0c3442f69b35f9642
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-17 18:20:25 +0300

    Add homework_3

---
+++ 3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите первое число - ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число - ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ваши числа - " + numberA + " " + numberB + " " + numberC);
+
+int maxNumber = numberA;
+
+if (numberA == numberB && numberA == numberC && numberB == numberC)
+{
+    Console.WriteLine("Введенные числа равны!");
+}
+else
+{
+    if (maxNumber < numberB)
+    {
+        maxNumber = numberB;
+    }
+    
+    if (maxNumber < numberC)
+    {
+        maxNumber = numberC;
+    }
+   
+    Console.WriteLine("Наибольшее число " + maxNumber);
+}
