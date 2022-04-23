18d541fd7c7e4c9ab915a9970b3a76255f751556
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-23 10:10:42 +0300

    Add description for HomeWork_3\3

--- HomeWork_3/3/Program.cs
+++ HomeWork_3/3/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите число N - ");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N - ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int index = 1;
