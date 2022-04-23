e55d0370df554b43486072fa6f1cf4f343d298bc
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-23 11:18:36 +0300

    Add HomeWork_4\1

---
+++ HomeWork_4/1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("В какую степень возводим первое число? Ввод - ");
+int powNumber = Convert.ToInt32(Console.ReadLine());
+
+if (powNumber >= 1)
+{
+    Console.WriteLine($"Число {number} в степени {powNumber} равно {Check(number, powNumber)}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод степени для числа!");
+}
+
+double Check (int number, int powNumber)
+{
+    double result = number;
+    for (int i = 1; i < powNumber; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
