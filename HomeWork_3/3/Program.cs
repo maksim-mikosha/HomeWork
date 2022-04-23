679fe7c743ec3f07d310f815f92da5bd8733b477
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-23 10:09:00 +0300

    Add HomeWork_3\3

---
+++ HomeWork_3/3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число N - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+
+if (number <= 0)
+{
+    number = -number;
+}
+
+while (number >= index)
+{
+    double result = Square(index);
+    Console.Write(result + " ");
+    index ++;
+}
+
+double Square(int number)
+{
+    return Math.Pow(number, 3);
+    // return number*number*number;       //Второй способ, но первый выглядит симпатичнее и не так топорно
+}
