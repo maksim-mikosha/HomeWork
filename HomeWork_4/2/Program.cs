d46d50918b665d2ba48720d6038b16d844af2785
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-23 12:06:46 +0300

    Add HomeWork_4\2

---
+++ HomeWork_4/2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число - ");
+string numberString = Console.ReadLine();
+
+int result = 0;
+
+for (int i = 0; i < numberString.Length; i++)
+{
+    result += Convert.ToInt32(numberString[i].ToString());
+}
+
+Console.WriteLine($"Сумма цифр в числе {numberString} равна {result}");
