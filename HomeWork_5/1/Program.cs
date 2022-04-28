b4638f88de7a46290acdac8034e24e8a3ba1468a
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-28 22:44:45 +0300

    Add HomeWork_5\1

---
+++ HomeWork_5/1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+Console.Write("Введите длину массива - ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = GetArray(length);
+int countEvenNumbers = CheckEvenNumbers(arrayNumbers);
+
+Console.Write("Случайно сгенерированный массив -");
+WriteArray(arrayNumbers);
+Console.Write($"Количество четных чисел в массиве - {countEvenNumbers}");
+
+int CheckEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] GetArray(int length)
+{
+    int[] newArray = new int[length];
+    FillArray(newArray);
+    return newArray;
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
