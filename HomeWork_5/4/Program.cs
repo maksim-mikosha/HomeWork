20da69056855d79fb37ae54c41ad008841aa361d
Maksim_Mikosha <mikmaksim@gmail.com>
2022-04-28 23:37:19 +0300

    Add HomeWork_5\4 - additional exercise from teacher

---
+++ HomeWork_5/4/Program.cs
@@ -0,0 +1,28 @@
+﻿//Доп.задание - В переменной string есть секретное сообщение, во второй есть пароль. Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
+
+Console.WriteLine("Введи пароль!");
+
+string secretString = "password";
+int countTry = 1;
+
+while (countTry <= 3)
+{
+    Console.Write("Угадывай! Попытка № " + countTry + " - ");
+    string userString = Console.ReadLine();
+    if (userString == secretString)
+    {
+        Console.WriteLine("Поздравляю! Ты справился :)");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Попробуй еще.");
+    }
+    countTry++;
+}
+
+if (countTry > 3)
+{
+    Console.WriteLine("Попытки закончились. Удачи в следующий раз.");
+    Console.WriteLine("Пароль - " + secretString);
+}
