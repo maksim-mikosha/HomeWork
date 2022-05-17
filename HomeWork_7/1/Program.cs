9c6bb5daadd5e78be02ab1b7820c41a0116b9979
Maksim_Mikosha <mikmaksim@gmail.com>
2022-05-17 21:08:42 +0300

    Add HomeWork_7\1

---
+++ HomeWork_7/1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int rowArray = Input("Введите количество строк в массиве - ");
+int columnsArray = Input("Введите количество столбцов в массиве - ");
+
+double[,] arrayOfDouble = new double[rowArray, columnsArray];
+
+FillArray(arrayOfDouble);
+PrintArray(arrayOfDouble);
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < rowArray; i++)
+    {
+        for (int j = 0; j < columnsArray; j++)
+        {
+            arrayOfDouble[i, j] = Math.Round(NewRandomDouble(-10.0, 10.0), 1);
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < rowArray; i++)
+    {
+        for (int j = 0; j < columnsArray; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double NewRandomDouble(double minimum, double maximum)
+{
+    Random random = new Random();
+    return random.NextDouble() * (maximum - minimum) + minimum;
+}
+
+int Input(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
